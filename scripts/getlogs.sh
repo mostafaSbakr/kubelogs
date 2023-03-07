3bdeae306921ee4a6543e2e3c13d1e84beda5a62
mostafaSbakr <mostafa.aboubakr@prodyna.com>
2023-03-07 09:45:29 +0100

    initial commit

---
+++ scripts/getlogs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kubectl config use-context $1
+
+pods=$(kubectl get pods -n $2 -o name)
+
+GREEN=$'\e[0;32m'
+RED=$'\e[0;31m'
+NC=$'\e[0m'
+
+for pod in $pods
+do
+    echo "${RED}pod logs${NC} ${GREEN}$pod${NC}"
+    kubectl logs -n $2 $pod | grep -i exception
+done
